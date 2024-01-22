6e46a219d24f016e67b8bc4ace87ab289e4e2318
Samar <osmansamar367@gmail.com>
2024-01-22 14:52:00 +0100

    Updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import reguests from "../Requests";
+import { FaPlayCircle } from "react-icons/fa";
+import { MdWatchLater } from "react-icons/md";
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
@@ -39,14 +41,24 @@ const Main = () => {
         />
         <div className="absolute w-full top-[20%] p-4 md:p-8">
           <h1 className="text-3xl md:text-5xl font-bold ">{movie?.title}</h1>
+
           <div className="my-4">
             <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
-              Play
+              <span className="inline-block align-middle">Play</span>
+              <FaPlayCircle
+                className="inline-block align-middle ml-1"
+                size={24}
+              />
             </button>
             <button className="border  text-white border-gray-300 py-2 px-5 ml-4">
-              Watch Later
+              <span className="inline-block align-middle"> Watch Later</span>
+              <MdWatchLater
+                className="inline-block align-middle ml-1 "
+                size={24}
+              />
             </button>
           </div>
+
           <p className="text-gray-400 text-sm">
             Released: {movie?.release_date}
           </p>
